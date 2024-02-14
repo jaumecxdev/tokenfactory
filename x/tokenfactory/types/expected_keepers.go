1d9cae869cf8c0b5098fe2902bec05994207a53a
Jaume <jaumecx@gmail.com>
2024-02-14 12:47:46 +0100

    Add minting and sending functionality

--- x/tokenfactory/types/expected_keepers.go
+++ x/tokenfactory/types/expected_keepers.go
@@ -7,19 +7,25 @@ import (
 )
 
 // AccountKeeper defines the expected interface for the Account module.
+// /home/jaume/go/pkg/mod/github.com/cosmos/cosmos-sdk@v0.50.3/x/bank/types/expected_keepers.go
 type AccountKeeper interface {
-	GetAccount(context.Context, sdk.AccAddress) sdk.AccountI // only used for simulation
+	GetAccount(ctx context.Context, addr sdk.AccAddress) sdk.AccountI
+	//GetAccount(context.Context, sdk.AccAddress) sdk.AccountI // only used for simulation
 	// Methods imported from account should be defined here
-	//GetModuleAddress(name string) sdk.AccAddress
+	GetModuleAddress(moduleName string) sdk.AccAddress
 	//GetModuleAccount(ctx sdk.Context, moduleName string) sdk.ModuleAccountI
+	GetModuleAccount(ctx context.Context, moduleName string) sdk.ModuleAccountI
 }
 
 // BankKeeper defines the expected interface for the Bank module.
 type BankKeeper interface {
-	SpendableCoins(context.Context, sdk.AccAddress) sdk.Coins
+	SpendableCoins(ctx context.Context, addr sdk.AccAddress) sdk.Coins
+	//SpendableCoins(context.Context, sdk.AccAddress) sdk.Coins
 	// Methods imported from bank should be defined here
 	//SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoins(ctx context.Context, from, to sdk.AccAddress, amt sdk.Coins) error
 	//MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	MintCoins(ctx context.Context, moduleName string, amt sdk.Coins) error
 }
 
 // ParamSubspace defines the expected Subspace interface for parameters.
